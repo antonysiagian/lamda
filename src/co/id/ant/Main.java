53f812acf3384174fa6abee87a6c59e800f78db4
Antony Siagian <antonysiagian@gmail.com>
2017-07-29 13:59:01 +1000

    Add lambda sample with functional interface

---
+++ src/co/id/ant/Main.java
@@ -0,0 +1,69 @@
+package co.id.ant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	private static List<String> listOfElements;
+
+	public static void main(String[] args) {
+
+		prepareList();
+		
+		testUsingNativeJavaExpression();
+		
+		testUsingLambdaExpression();
+
+	}
+
+	
+	private static void testUsingNativeJavaExpression() {
+		Searchable searchable = new Searchable() {
+			@Override
+			public String search(List<String> elements, String textToSearch) {
+				for (int i = 0; i < elements.size(); i++) {
+					if (elements.get(i).equals(textToSearch)) {
+						return elements.get(i) + " at index " + i;
+					}
+				}
+				return "";
+			}
+		};
+
+		System.out.println("Using native java code: " + searchable.search(listOfElements, "Antony"));
+		System.out.println("Using native java code: " + searchable.search(listOfElements, "Siagian"));
+	}
+
+	
+	private static void testUsingLambdaExpression() {
+
+		Searchable lambdaSearchable = (List<String> elements, String textToSearch) -> {
+			int i = 0;
+			for (String text : elements) {
+				if (textToSearch.equals(text)) {
+					return text + " at index " + i;
+				}
+				i++;
+			}
+			return "";
+		};
+
+		System.out.println("Using lambda expression: " + lambdaSearchable.search(listOfElements, "Joko"));
+		System.out.println("Using lambda expression: " + lambdaSearchable.search(listOfElements, "Widodo"));
+	}
+
+	
+	private static void prepareList() {
+		listOfElements = new ArrayList<String>();
+		listOfElements.add("Antony");
+		listOfElements.add("Joko");
+		listOfElements.add("Widodo");
+		listOfElements.add("Siagian");
+	}
+
+}
+
+interface Searchable {
+	String search(List<String> elements, String textToSearch);
+}
